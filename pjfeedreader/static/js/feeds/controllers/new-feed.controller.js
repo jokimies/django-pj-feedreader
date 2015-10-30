1c1eed84bad9c1522d970c124cd5f0d66f1f1276
Petri Jokimies <petri.jokimies@iki.fi>
2015-10-30 12:16:26 +0200

    feat(feed): Add submit function, use new sercives
    Added submit() function to be able to add new feeds.
    Use refactored services (53f8513 and 4eafb6e).

--- pjfeedreader/static/js/feeds/controllers/new-feed.controller.js
+++ pjfeedreader/static/js/feeds/controllers/new-feed.controller.js
@@ -6,9 +6,9 @@
     'use strict';
 
     angular
-	.module('pjfeedreader.feeds.controllers')
-	.controller('NewFeedController', [ '$rootScope', '$scope', 'Feeds',
-					   'Categories',NewFeedController]);
+        .module('pjfeedreader.feeds.controllers')
+        .controller('NewFeedController', [ '$rootScope', '$scope', 'Feeds',
+                                           'Categories',NewFeedController]);
     
     /**
      * @namespace NewFeedController
@@ -16,76 +16,41 @@
 
     function NewFeedController($rootScope, $scope, Feeds, Categories) {
 
-	var self = this;
+        var self = this;
 
-	self.submit = submit;
-	self.newFeed = {} ;
+        self.submit = submit;
+        self.newFeed = {} ;
 
-	self.categories = []
+        self.categories = [];
 
-	Categories.all().then(allCategorySuccessFn, allCategoryErrorFn)
-	console.log('NFCtrl')
-	console.log(self.categories)
+        Categories.all().then(function(data) {
+            self.categories = data.data;
+        });
+
         /**
          * @name allCategorySuccessFn
          * @desc Show success message
          */
 
-	function allCategorySuccessFn(data, status, headers, config) {
-	    
-	    self.categories = data.data
-	}
-	
+
         /**
-         * @name allCategoryErrorFn
-         * @desc Show success message
+         * @name submit
+         * @desc Create a new Feed
+         * @memberOf pjfeedreader.feeds.controllers.NewFeedController
          */
+        function submit() {
 
-	function allCategoryErrorFn(data, status, headers, config) {
-	    console.log('All Category error');
-	}
+            /* Tell listeners (e.g. IndexController) that a new feed was
+            added */
+            $rootScope.$broadcast('feed.added', {
+                feed: self.newFeed,
+                category: self.selectedCategory
+            });
+            $scope.closeThisDialog();
+            Feeds.add(self.newFeed.url, 'Default title', 
+                      self.selectedCategory);
 
-
-	/**
-	 * @name submit
-	 * @desc Create a new Feed
-	 * @memberOf pjfeedreader.feeds.controllers.NewFeedController
-	 */
-	function submit() {
-	    console.log('Add submit pressed');
-
-	    /* Tell listeners (e.g. IndexController) that a new feed was
-	    added */
-	    $rootScope.$broadcast('feed.added', {
-		feed: self.newFeed,
-		category: self.selectedCategory
-	    });
-	    $scope.closeThisDialog();
-	    Feeds.add(self.newFeed.url, 'Default title', 
-		      self.selectedCategory)
-		.then(addFeedSuccessFn, 
-		      addFeedErrorFn); 
-
-	    /**
-	     * @name addFeedSuccessFn
-	     * @desc Show success message
-	     */
-
-	    function addFeedSuccessFn(data, status, headers, config) {
-		console.log('Adding feed succesfully');
-	    }
-
-	    /**
-	     * @name addFeedErrorFn
-	     * @desc Show success message
-	     */
-
-	    function addFeedErrorFn(data, status, headers, config) {
-		console.log('Error in adding')
-		console.log(data)
-		console.log(data.error);
-	    }
-	}
+        }
     }
 })();
 	
