49e33fe233f412f187a6920de3eb0dc39f1273aa
Petri Jokimies <petri.jokimies@iki.fi>
2015-12-05 18:47:22 +0200

    fix(): Replace Google API. Fixes #2
    Google shutdown its feeds API. Replaced with Yahoo's.

--- pjfeedreader/static/js/layout/controllers/index.controller.js
+++ pjfeedreader/static/js/layout/controllers/index.controller.js
@@ -14,7 +14,7 @@
     function IndexController($scope, $http, $timeout, Feeds, Categories) {
 
         var self = this;
-        self.refreshInterval = 300;
+        self.refreshInterval = 600;
         self.feeds = [];
         self.categories = [];
         self.slicedFeeds = [];
@@ -35,18 +35,48 @@
 
         function fetchFeed(feed) {
             feed.items = [];
-            var url = '//ajax.googleapis.com/ajax/services/feed/load?v=1.1&num=10&callback=JSON_CALLBACK&q=' + encodeURIComponent(feed.url);
+
+            var format = '&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=JSON_CALLBACK';
+            var query = "select * from xml where url = '" + feed.url + "'"
+            var url = 'http://query.yahooapis.com/v1/public/yql?q=' + 
+                fixedEncodeURIComponent(query) + format;
+            var feedItem
+            var what = Object.prototype.toString
 
             $http.jsonp(url).
                 success(function(data, status, headers, config) {
-                    feed.items=data.responseData.feed.entries;
-                    feed.title=data.responseData.feed.title;
+                    /* Yahoo API returns an array in channel.item, if
+                       there's more than one item in the feed, otherwise it
+                       return an object
+                    */
+                    feedItem = data.query.results.rss.channel.item
+
+                    /* feed.items is supposed to be an array. If array is
+                       returned use it as such, if not push the single item
+                       to feed.items -array 
+                    */
+                    if ( what.call(feedItem) === '[object Array]') {
+                        feed.items=data.query.results.rss.channel.item;
+                    } else {
+                        feed.items.push(data.query.results.rss.channel.item)
+                    }
+                    feed.title=data.query.results.rss.channel.title;
                 }).
                 error(function(data, status, headers, config) {
                     console.error('Error fetching feed:', data);
                 });
             $timeout(function() { fetchFeed(feed); }, self.refreshInterval * 1000);
         }
+
+        function fixedEncodeURIComponent(url) {
+            return encodeURIComponent(url).
+                replace(/[!'()]/g, escape). //'
+                replace(/\*/g, '%2A').
+                replace(/\"/g, '%22').
+                replace(/:/g, '%3A').
+                replace(/\//g, '%2F');
+
+        };
 
         function addFeed(feed) {
             fetchFeed(feed);
