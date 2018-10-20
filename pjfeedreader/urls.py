33db53e0a06240a56e2cee743dbe4f2c3a5f0ff3
Petri Jokimies <petri.jokimies@iki.fi>
2018-10-20 11:20:05 +0300

    Remove deprecation warning on urls
    Fixed
          RemovedInDjango110Warning: django.conf.urls.patterns() is deprecated and
          will be removed in Django 1.10. Update your urlpatterns to be a list of
          django.conf.urls.url() instances instead.
    warning.

--- pjfeedreader/urls.py
+++ pjfeedreader/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 
 from rest_framework.routers import SimpleRouter
 from .views import FeedViewSet, CategoryViewSet, FeedIndexView
@@ -8,9 +8,8 @@ router.register(r'categories', CategoryViewSet)
 router.register(r'feeds', FeedViewSet)
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^api/v1/', include(router.urls)),
     #url(r'^$', FeedIndexView.as_view(), name='feed-index'),
     url(r'^.*', FeedIndexView.as_view(), name='feed-index'),
-    )
+]
